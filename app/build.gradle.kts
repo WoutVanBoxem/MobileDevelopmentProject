cfce3780a10f1e7be1bd481d0913a49e2acfb8b5
WoutVanBoxem <woutvanboxem02@gmail.com>
2023-11-16 10:23:18 +0100

    update android SDK
    android compile sdk aangepast om compatibel te zijn met de gekozen template

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mobiledevelopmentproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mobiledevelopmentproject"
