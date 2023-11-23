59e631e87a0d256111868345ef2b798ec3362936
WoutVanBoxem <woutvanboxem02@gmail.com>
2023-11-23 11:06:02 +0100

    added login backend

--- app/src/main/java/com/example/mobiledevelopmentproject/Login.kt
+++ app/src/main/java/com/example/mobiledevelopmentproject/Login.kt
@@ -2,10 +2,50 @@ package com.example.mobiledevelopmentproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
 
 class Login : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        val emailEditText = findViewById<TextInputEditText>(R.id.email)
+        val passwordEditText = findViewById<TextInputEditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.btn_login)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                loginUser(email, password)
+            } else {
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Navigeer naar MainActivity
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // Toon foutmelding
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
