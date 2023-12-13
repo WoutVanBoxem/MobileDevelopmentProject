77e130e0f357d9f8ed66305241334ee9723d0ac8
OmarMai <omar.mai@student.ap.be>
2023-12-13 01:04:45 +0100

    Profile Update

--- app/src/main/java/com/example/mobiledevelopmentproject/Profile.kt
+++ app/src/main/java/com/example/mobiledevelopmentproject/Profile.kt
@@ -9,6 +9,8 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.auth.FirebaseUser
 import android.util.Log
+import android.widget.Button
+import android.widget.Spinner
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Profile : AppCompatActivity() {
@@ -25,9 +27,13 @@ class Profile : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
 
+        val enableEditTextButton = findViewById<Button>(R.id.enableEditTextButton)
+        val saveEditTextButton = findViewById<Button>(R.id.saveEditTextButton)
         auth = FirebaseAuth.getInstance()
         val db = FirebaseFirestore.getInstance()
         val currentUser: FirebaseUser? = auth.currentUser
+
+
 
         if (currentUser?.email != null) {
             db.collection("users")
@@ -45,7 +51,20 @@ class Profile : AppCompatActivity() {
                         findViewById<TextView>(R.id.firstNameTextView).text = user?.firstname
                         findViewById<TextView>(R.id.lastNameTextView).text = user?.lastname
                         findViewById<TextView>(R.id.cityTextView).text = user?.city
-                        findViewById<TextView>(R.id.genderTextView).text = user?.gender
+                        // Assume user?.gender is a String representing the selected gender
+                        val genderSpinner = findViewById<Spinner>(R.id.spinner_gender)
+                        val genderArray = resources.getStringArray(R.array.gender_options)
+
+// Find the index of the user's gender in the array
+                        val selectedGenderIndex = genderArray.indexOf(user?.gender)
+
+// Set the selected item in the Spinner
+                        if (selectedGenderIndex != -1) {
+                            genderSpinner.setSelection(selectedGenderIndex)
+                        } else {
+                            // Handle the case where the user's gender is not found in the array
+                            Log.e(TAG, "User gender not found in gender options array")
+                        }
 
                         // Do something with the user object
                     } else {
@@ -56,6 +75,64 @@ class Profile : AppCompatActivity() {
                     // Handle errors
                     //Log.d(TAG, "Error getting documents: ", exception)
                 }
+        }
+
+        val editEmail = findViewById<TextView>(R.id.emailTextView)
+        val editAddress = findViewById<TextView>(R.id.addressTextView)
+        val editFirstName = findViewById<TextView>(R.id.firstNameTextView)
+        val editLastName = findViewById<TextView>(R.id.lastNameTextView)
+        val editCityName = findViewById<TextView>(R.id.cityTextView)
+        val editGender = findViewById<Spinner>(R.id.spinner_gender)
+        enableEditTextButton.setOnClickListener {
+            // Enable the EditText elements
+
+            editAddress.isEnabled = true
+            editFirstName.isEnabled = true
+            editLastName.isEnabled = true
+            editCityName.isEnabled = true
+            editGender.isEnabled = true
+        }
+        saveEditTextButton.setOnClickListener{
+
+            val collectionRef = db.collection("users")
+
+            saveEditTextButton.setOnClickListener {
+                val userEmail = editEmail.text.toString() // Replace with the user's email
+                val updates = mapOf(
+                    "address" to editAddress.text.toString(),
+                    "firstname" to editFirstName.text.toString(),
+                    "city" to editCityName.text.toString(),
+                    "lastname" to editLastName.text.toString(),
+                    "gender" to editGender.selectedItem.toString()
+
+                    // Add other fields as needed
+                )
+
+                collectionRef.whereEqualTo("email", userEmail).limit(1)
+                    .get()
+                    .addOnSuccessListener { documents ->
+                        if (!documents.isEmpty) {
+                            val documentId = documents.documents[0].id
+
+                            // Update the document with the new values
+                            collectionRef.document(documentId).update(updates)
+                                .addOnSuccessListener {
+                                    // Document successfully updated
+                                    Log.d(TAG, "DocumentSnapshot successfully updated!")
+                                }
+                                .addOnFailureListener { e ->
+                                    // Handle errors
+                                    Log.w(TAG, "Error updating document", e)
+                                }
+                        } else {
+                            // Document with the specified email not found
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        // Handle errors
+                        Log.w(TAG, "Error getting documents: ", exception)
+                    }
+            }
         }
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.navigation)
